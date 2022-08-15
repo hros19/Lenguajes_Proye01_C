8fe4cdec9df498225c01f3aff128a50f4be223b2
Alex Sánchez <asmalexander@hotmail.com>
2022-08-15 02:38:55 -0600

    Desarrollo de menus del programa

---
+++ PRY1/src/PRY1.c
@@ -0,0 +1,249 @@
+/*****Datos administrativos************************
+* Nombre del archivo:PRY1
+* Tipo de archivo:C
+* Proyecto: Sistema de produccion agricola
+* Autor: Alex Sánchez Céspedes - Hansol Antay
+* Empresa: Instituto Tecnológico de Costa Rica
+*****Descripción******************************************
+* Programa que realiza una gestion de produccion agricola
+* Desarrollada en lenguaje C con conexion local a MySQL DB
+*****Versión**********************************************
+* ## | Fecha y hora | Autor
+* 14/08/2022
+**************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+
+#define BUFFER 50
+
+void Menu_01();
+void Menu_OP1();
+void Menu_OA1();
+void Menu_VI01();
+void Menu_RN01();
+void Menu_RV01();
+void Menu_CN();
+void Menu_CV01();
+void Menu_BA01();
+bool VerificarNumero(char num[]);
+
+
+/*****Nombre***************************************
+* main
+*****Descripción**********************************
+* Metodo de inicio del programa
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+int main() {
+    //revisar lo del fullscreen
+    system ("clear");
+    Menu_01();
+    return 0;
+}
+
+
+/*****Nombre***************************************
+* Menu_01
+*****Descripción**********************************
+* Menu principal del programa
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+void Menu_01() {
+    printf("GESTIÓN DE PRODUCCIÓN AGRÍCOLA [GPA]");
+    char opcion_01[BUFFER];
+    printf("\n1.Opciones operativas\n2.Opciones administrativas\n>");
+    scanf("%s",&opcion_01);
+    if (!VerificarNumero(opcion_01)) {
+        printf("[ERROR] = POR FAVOR DIGITE UNA DE LAS OPCIONES DISPONIBLES!");
+        printf("[PRESIONE CUALQUIER TECLA PARA CONTINUAR!!!]\n\n");
+        //poner un pause pause();
+        Menu_01();
+    }
+    int opcion_02 = strtol(opcion_01,NULL,10);
+    switch (opcion_02) {
+        case 1:
+            Menu_OP1();
+            break;
+        case 2:
+            Menu_OA1();
+            break;
+        default:
+            printf("[ERROR] = POR FAVOR DIGITE UNA DE LAS OPCIONES DISPONIBLES!");
+            printf("[PRESIONE CUALQUIER TECLA PARA CONTINUAR!!!]\n\n");
+            //poner pause;
+            break;
+    }
+
+}
+
+
+/*****Nombre***************************************
+* Menu_OP1()
+*****Descripción**********************************
+* Menu de gestion de opciones operativas
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+void Menu_OP1() {
+    system("clear");
+    printf("[MENÚ DE OPCIONES OPERATIVAS]\n");
+    //se solicita la contraseña
+    printf("Ingrese el nombre de usuario y contraseña\nNombre de usuario:");
+    char* nombreUsuario;
+    scanf("%s",&nombreUsuario);
+    //validamos nombre de nombreUsuario
+    printf("Contraseña:");
+    char* contraseña;
+    scanf("%s",&contraseña);
+    //validamos contraseña
+    
+}
+
+
+/*****Nombre***************************************
+* Menu_OA1
+*****Descripción**********************************
+* Menu de gestion de opciones administrativas
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+void Menu_OA1() {
+    system("clear");
+    printf("MENU DE OPCIONES ADMINISTRATIVAS\n");
+    printf("1.Valores iniciales\n2.Registro de nomina\n3.Registro de venta de productos\n4.Consulta de nominas\n5.Consulta de ventas\n6.Valance anual\n7.Volver atras\n8.Salir\n>");
+    char opcion_01[BUFFER];
+    scanf("%s",&opcion_01);
+    if (!VerificarNumero(opcion_01)) {
+        printf("[ERROR] = POR FAVOR DIGITE UNA DE LAS OPCIONES DISPONIBLES!");
+        printf("[PRESIONE CUALQUIER TECLA PARA CONTINUAR!!!]\n\n");
+        //poner un pause
+        Menu_OA1();
+    }
+    int opcion_02 = strtol(opcion_01,NULL,10);
+    switch(opcion_02) {
+        case 1:
+            Menu_VI01();
+            break;
+        case 2:
+            Menu_RN01();
+            break;
+        case 3:
+            Menu_RV01();
+            break;
+        case 4:
+            Menu_CN();
+            break;
+        case 5:
+            Menu_CV01();
+            break;
+        case 6:
+            Menu_BA01();
+            break;
+        case 7:
+            Menu_01();
+            break;
+        case 8:
+            return;
+        default:
+            printf("[ERROR] = POR FAVOR DIGITE UNA DE LAS OPCIONES DISPONIBLES!");
+            printf("[PRESIONE CUALQUIER TECLA PARA CONTINUAR!!!]\n\n");
+            //poner un pause;
+            Menu_OA1();
+    }
+}
+/*****Nombre***************************************
+* Menu_VI01
+*****Descripción**********************************
+* Menu de gestion de valores iniciales
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+void Menu_VI01() {}
+
+/*****Nombre***************************************
+* Menu_RN01
+*****Descripción**********************************
+* Menu de gestion de regitro de nominas
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+void Menu_RN01() {}
+
+/*****Nombre***************************************
+* Menu_RV01
+*****Descripción**********************************
+* Menu de gestion de registro de ventas
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+void Menu_RV01() {}
+
+/*****Nombre***************************************
+* Menu_CN
+*****Descripción**********************************
+* Menu de consulta de nominas
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+void Menu_CN() {}
+
+/*****Nombre***************************************
+* Menu_CV01
+*****Descripción**********************************
+* Menu de consulta de ventas
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+void Menu_CV01() {}
+
+/*****Nombre***************************************
+* Menu_VA01
+*****Descripción**********************************
+* Menu de gestion de balance anual
+*****Retorno**************************************
+* Sin retorno 
+*****Entradas*************************************
+* Sin entradas
+**************************************************/
+void Menu_BA01() {}
+
+
+
+/*****Nombre***************************************
+* VerificarNumero
+*****Descripción**********************************
+* Programa que valida si la entrada es un numero
+*****Retorno**************************************
+* Valor booleano 
+*****Entradas*************************************
+* Cadena de caracteres
+**************************************************/
+bool VerificarNumero(char num[]) {
+    for(int i = 0; i < strlen(num); i++) {if(isdigit(num[i])==0) return false;}
+    return true;
+}
+
